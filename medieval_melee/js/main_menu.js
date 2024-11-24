ce78ca67dc34e22ffcd519ee38c081dec985ed52
Sandstorm <57902127+sandstormdeveloper@users.noreply.github.com>
2024-11-24 22:52:31 +0100

    Menú básico

--- medieval_melee/js/main_menu.js
+++ medieval_melee/js/main_menu.js
@@ -4,11 +4,28 @@ class MainMenuScene extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('menu', 'assets/menu.png');
+        this.load.image('titulo', 'assets/titulo.png');
+        this.load.image('titulo', 'assets/titulo.png');
+        this.load.image('play', 'assets/play.png');
+        this.load.image('play_hover', 'assets/play_hover.png');
     }
 
     create() {
+        this.add.image(640, 360, 'menu');
+        this.add.image(640, 200, 'titulo');
 
+        var start_button = this.add.image(640, 350, 'play')
+            .setInteractive()
+            .on('pointerover', () => {
+                start_button.setTexture("play_hover");
+            })
+            .on('pointerout', () => {
+                start_button.setTexture("play");
+            })
+            .on('pointerdown', () => {
+                this.scene.start('GameScene');
+        });    
     }
 
     update(time, delta) {
