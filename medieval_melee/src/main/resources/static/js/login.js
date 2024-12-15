19b6d67cf7de48c0f7fbea00340a3dd7f0e71e40
Sandstorm <57902127+sandstormdeveloper@users.noreply.github.com>
2024-12-15 14:53:18 +0100

    Escena de login (sin API REST)

---
+++ medieval_melee/src/main/resources/static/js/login.js
@@ -0,0 +1,50 @@
+class LoginScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'LoginScene' });
+    }
+
+    preload() {
+        this.load.image('fondo_login', 'assets/castillo.png');
+        this.load.html('login', 'login.html');
+    }
+
+    create() {
+        // Efecto de fade-in al entrar en la escena
+        this.cameras.main.fadeIn(500, 0, 0, 0);
+
+        this.add.image(640, 360, 'fondo_login');
+
+        var form = this.add.dom(640, 360).createFromCache('login').setOrigin(0.5);
+
+        form.addListener('click');
+
+        form.on('click', (event) => { 
+            if (event.target.name === 'loginButton') {
+                const inputUsername = form.getChildByName('username'); 
+                const inputPassword = form.getChildByName('password');
+        
+                if (inputUsername.value !== '' && inputPassword.value !== '') {
+                    form.removeListener('click'); 
+                    
+                    //form.scene.tweens.add({ targets: form.rotate3d, x: 1, w: 90, duration: 2000, ease: 'Power3' });
+
+                    form.scene.tweens.add({
+                        targets: form, scaleX: 0, scaleY: 0, duration: 500, ease: 'Power3',
+                    });
+
+                    this.nextScene(); 
+                }
+            }
+        });
+        
+    }
+
+    nextScene() {
+        this.cameras.main.fadeOut(1000, 0, 0, 0);
+
+        // Espera a que el fade-out termine antes de iniciar la nueva escena
+        this.cameras.main.once('camerafadeoutcomplete', () => {
+            this.scene.start('MainMenuScene');
+        });
+    }
+}
